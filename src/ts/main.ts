67b13d83a2e14510cf74ec74d7bc94c4a843577d
losalmson <mialo.salmson@gmail.com>
2023-12-06 10:05:40 +0100

    Added vite config file

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,5 +1,47 @@
 import './../scss/style.scss'
 
+function myAwesomeFunction() {
+  const numbers: number[] = [9, 8, 1, 2, 0, 3, 8, 1, 2]; 
+
+  for (let i = 0; i < numbers.length; i ++) {
+    if (i % 2 === 0) {
+      numbers[i] *= 2;
+    } else {
+    numbers[1] *= 1; 
+  }
+}
+console.log(numbers);
+}
+myAwesomeFunction();
+
+/*
+Övnig 1 med TS
+
+function sum (x: number, y: number): number {
+  let theResult: number = x + y; 
+  return theResult;
+}
+
+const totalSum = sum(5, 10);
+console.log(totalSum);
+
+Övning 1.1 
+
+function myFirstTypeScript() {
+const name: string = "Lo Salmson";
+
+const x: number = 5;
+const y: number = 10;
+
+const sum: number = x + y;
+
+console.log(name);
+console.log(sum);
+}
+myFirstTypeScript();
+
+Övning 1
+
 function myFirstTypeScript() {
   if (confirm ("Are you sure?") === true) {
     alert("You pressed OK!");
@@ -7,4 +49,4 @@ function myFirstTypeScript() {
     alert("You canceled!");
   }
 }
-myFirstTypeScript();
+myFirstTypeScript(); */
